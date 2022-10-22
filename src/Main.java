e7ef331bd14c7d5d3e1b9b6980ddfcac97ddb69d
TrevorRau <trau@elon.edu>
2022-10-22 17:38:58 -0400

    add step

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,32 @@
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.*;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class Main extends Application {
 	public static final int SCREEN_WIDTH = 1000;
 	public static final int SCREEN_HEIGHT = 500;
 	public static final Paint SCREEN_COLOR = Color.BLACK;
+	public static final int FRAMES_PER_SECOND = 60;
+	public static final int MILLISECOND_DELAY = 1000 / FRAMES_PER_SECOND;
+	public static final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
 	
 	@Override
 	public void start(Stage stage) {
 		stage.setScene(CreateScene(SCREEN_WIDTH,SCREEN_HEIGHT,SCREEN_COLOR));
-		stage.setTitle("Breakout");
+		stage.setTitle("Arcade");
 		stage.show();
+		
+		KeyFrame frame = new KeyFrame(Duration.millis(MILLISECOND_DELAY), e -> step(SECOND_DELAY, stage));
+		Timeline animation = new Timeline();
+		animation.setCycleCount(Timeline.INDEFINITE);
+		animation.getKeyFrames().add(frame);
+		animation.play();
 	}
 	
 
@@ -24,6 +36,10 @@ public class Main extends Application {
 		Scene myScene = new Scene(root, sceneWidth, sceneHeight, background);
 		return myScene;
 				
+	}
+	
+	private void step (double elapsedTime, Stage stage) {
+		
 	}
 	
 	public static void main(String[] args) {
