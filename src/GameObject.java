735fd870522873b92458941a5f6f31f0be076f43
William Zinkann <wzinkann@gmail.com>
2022-10-26 08:57:21 -0400

    First draft of what Game Object will look like

--- src/GameObject.java
+++ src/GameObject.java
@@ -1,14 +1,66 @@
-import javafx.scene.Group;
-import javafx.scene.Node;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Random;
 
-//how do we want to construct this 
+
+import javafx.geometry.Bounds;
+import javafx.geometry.Point2D;
+import javafx.scene.Node;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+/**
+ * Abstract class to create all game objects
+ *
+ * @author Will Zinkann
+ */ 
 public abstract class GameObject {
 	
-	abstract void move();
+	protected ImageView myView;
+
 	
 	
-	abstract Node getView();
+	// 
+	public GameObject(String imagePath, double sizeWidth, double sizeHeight, Point2D pos) throws FileNotFoundException {
+		Image image = new Image(new FileInputStream(imagePath));
+        myView = new ImageView(image);
+        
+        myView.setFitWidth(sizeWidth);
+        myView.setFitHeight(sizeHeight);
+
+        myView.setX(pos.getX());
+        myView.setY(pos.getY());
+
+	}
+	
+	protected int getRandomInRange (int min, int max) {
+		Random dice = new Random();
+        return min + dice.nextInt(max - min) + 1;
+    }
+
+	
+	public Node getView() {
+		return myView;
+	}
 	
 	
+	public boolean isIntersecting(GameObject other) {
+		return this.getBounds().intersects(other.getBounds());
+	}
 	
+	public Bounds getBounds() {
+		return myView.getBoundsInParent();
+	}
+
+
+	@Override
+	public String toString() {
+		return "GameObject [myView=" + myView + "]";
+	}
+
+
+
+
+
+
 }
